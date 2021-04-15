0168394386f9419b63202c4eaf848ebca52fdfc2
borecjeborec1 <robingrundel@seznam.cz>
2021-04-15 08:56:16 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const subForm = document.getElementById('input1');
+const topForm = document.getElementById('input2');
+const form = document.getElementById('form');
+const scrapBtn = document.getElementById('scrapBtn');
+const svg = document.getElementById('svg');
+const resMsg = document.getElementById('resMsg');
+const downloadBtn = document.getElementById('downloadBtn');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (subForm.value && topForm.value) {
+    postNotes(subForm.value, topForm.value);
+    resMsg.style.display = 'block';
+    svg.style.display = 'block';
+    scrapBtn.disabled = true;
+    setTimeout(() => {
+      resMsg.innerText = 'Download the .txt file';
+      svg.style.display = 'none';
+      downloadBtn.style.display = 'block';
+      subForm.value = '';
+      topForm.value = '';
+    }, 5000);
+  }
+});
+
+async function postNotes(predmet, tema) {
+  await fetch('http://localhost:3000/api/create', {
+    method: 'POST',
+    body: JSON.stringify({ subject: predmet, topic: tema }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
